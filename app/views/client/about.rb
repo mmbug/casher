a5d691668f2e33a915f14fa4057e494ca3fd6ddd
mmbug <mmbug256@gmail.com>
2019-06-08 16:43:08 -0400

    Version bump to 1.0.1880

---
+++ app/views/client/about.rb
@@ -0,0 +1,7 @@
+Nickname *@#{hb_client.username}*
+Balance *UAH 1200.8*
+Referals *12 people*
+Earnings *UAH 121.7*
+
+****
+[[button('Receive BTC', 'receive_btc', 1), button('Send BTC', 'send_btc', 1)]]
