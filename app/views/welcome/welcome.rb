d2af1f0dd89a85f4aca40c33230c271e8bf4c719
mmbug <mmbug256@gmail.com>
2019-06-08 15:55:56 -0400

    Version bump to 1.0.1863

---
+++ app/views/welcome/welcome.rb
@@ -0,0 +1,15 @@
+🤝 *Client Agreement*
+
+*#{BOT_NAME}* is a peer-to-peer platform service that enables
+its users to exchange currency to and from bitcoin. Users can use
+the platform to create advertisements where they offer to buy or sell bitcoin.
+
+Other users reply to these advertisements and the two users can agree to meet to trade bitcoins for cash or complete the trade
+online. In order for a user to sell bitcoins they (`seller`) must store
+the bitcoins in the *#{BOT_NAME}*  web wallet belonging to their account from where the bitcoins are transferred to the user
+purchasing the bitcoins (`buyer`) after the seller confirms
+the buyers payment.
+
+All trades on this site are conducted between users of the service. *#{BOT_NAME}*  may help to resolve disputes between buyers and sellers. Acting as an escrow provider, *#{BOT_NAME}* is not itself a party of any bitcoin trade or transactions conducted by its users.
+****
+[['👌 I agree']]
