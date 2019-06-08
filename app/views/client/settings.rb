941c09eb74348599642613c0b4ff363774300d72
mmbug <mmbug256@gmail.com>
2019-06-08 17:31:15 -0400

    Version bump to 1.0.1901

---
+++ app/views/client/settings.rb
@@ -0,0 +1,17 @@
+*#{BOT_NAME}* is a peer-to-peer platform service that enables
+its users to exchange currency to and from bitcoin. Users can use
+the platform to create advertisements where they offer to buy or sell bitcoin.
+
+Please choose what kind of information you would like to know about us and click the corresponding button.
+****
+[
+    [
+      button('Forums', 'receive_btc', 1),
+      button('crx24.io', 'receive_btc', 1),
+    ],
+    [
+      button('Facebook', 'receive_btc', 1),
+      button('Twitter', 'send_btc', 1),
+      button('Telegram', 'send_btc', 1)
+    ]
+]
