23b011dc98872a3ef48e0718f9b18e87a1533b5d
mmbug <mmbug256@gmail.com>
2019-06-14 12:54:50 -0400

    Version bump to 1.0.2435

---
+++ app/views/client/trade/choose_buyer.rb
@@ -0,0 +1,9 @@
+*🎨  Продать*
+
+Вы собираетесь продать `BTC` на сумму #{usd(sget('amount'))} через электронную систему *#{Meth[sget('method')].title}*. Выберите покупателя из списка.
+****
+buts ||= []
+@avlbl = hb_client.available_sellers
+buts = keyboard(@avlbl, 2) do |rec|
+    button("#{rec[:username]}, #{rec[:rate]} USD", 1)
+end
